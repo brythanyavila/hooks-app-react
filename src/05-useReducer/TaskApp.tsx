7c9bcbbe1b618c863ae374b9d938b045bab5af79
brythany avila <brythanyavila14@gmail.com>
2026-06-16 15:48:46 -0600

    Local Persistence - LocalStorage Object Validators - Zod Scrambled Words Scramble Reducer - Managing Complex State Reducer Logic Side Effects with a Reducer

--- src/05-useReducer/TaskApp.tsx
+++ src/05-useReducer/TaskApp.tsx
@@ -1,4 +1,4 @@
-import { useReducer, useState } from 'react';
+import { useEffect, useReducer, useState } from 'react';
 
 import { Plus, Trash2, Check } from 'lucide-react';
 
@@ -11,6 +11,10 @@ import { getTasksInitialState, taskReducer } from './reducer/tasksReducer';
 export const TaskApp = () => {
   const [inputValue, setInputValue] = useState('');
   const [state, dispatch] = useReducer(taskReducer, getTasksInitialState());
+
+  useEffect(() => {
+    localStorage.setItem('tasks-state', JSON.stringify(state));
+  }, [state]);
 
   const addTodo = () => {
     if (inputValue.length === 0) return;
--- src/05-useReducer/reducer/tasksReducer.ts
+++ src/05-useReducer/reducer/tasksReducer.ts
@@ -1,3 +1,5 @@
+import * as z from 'zod';
+
 interface Todo {
   id: number;
   text: string;
@@ -16,13 +18,47 @@ export type TaskAction =
   | { type: 'TOGGLE_TODO'; payload: number }
   | { type: 'DELETE_TODO'; payload: number };
 
+const TodoSchema = z.object({
+  id: z.number(),
+  text: z.string(),
+  completed: z.boolean(),
+});
+
+const TaskStateSchema = z.object({
+  todos: z.array(TodoSchema),
+  length: z.number(),
+  completed: z.number(),
+  pending: z.number(),
+});
+
 export const getTasksInitialState = (): TaskState => {
-  return {
-    todos: [],
-    completed: 0,
-    pending: 0,
-    length: 0,
-  };
+  const localStorageState = localStorage.getItem('tasks-state');
+
+  if (!localStorageState) {
+    return {
+      todos: [],
+      completed: 0,
+      pending: 0,
+      length: 0,
+    };
+  }
+
+  // Validat mediante zod
+  const result = TaskStateSchema.safeParse(JSON.parse(localStorageState));
+
+  if (result.error) {
+    console.log(result.error);
+    return {
+      todos: [],
+      completed: 0,
+      pending: 0,
+      length: 0,
+    };
+  }
+
+  //! Cuidado, porque el objeto puede haber sido manipulado
+  //   return JSON.parse(localStorageState);
+  return result.data;
 };
 
 export const taskReducer = (
