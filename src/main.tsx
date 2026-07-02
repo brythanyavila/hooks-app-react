2bc7b8f05715f8fbf24ca3590eec4ea013b20d00
brythany avila <brythanyavila14@gmail.com>
2026-07-02 13:49:16 -0600

    useOptimistic - Exercise Preparation useTransition Simulate a failure in useOptimistic + sonner

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Toaster } from 'sonner';
 
 // import { HooksApp } from './HooksApp'
 // import { TrafficLight } from './01-useState/TrafficLight'
@@ -10,12 +11,14 @@ import { createRoot } from 'react-dom/client';
 // import { TaskApp } from './05-useReducer/TaskApp';
 // import { ScrambleWords } from './05-useReducer/ScrambleWords';
 // import { MemoHook } from './06-memos/MemoHook';
-import { MemoCounter } from './06-memos/MemoCounter';
+// import { MemoCounter } from './06-memos/MemoCounter';
+import { InstagromApp } from './07-useOptimistic/InstagromApp';
 
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Toaster />
     {/* <HooksApp/> */}
     {/* <TrafficLight /> */}
     {/* <TrafficLightWithEffect /> */}
@@ -25,6 +28,7 @@ createRoot(document.getElementById('root')!).render(
     {/* <TaskApp /> */}
     {/* <ScrambleWords /> */}
     {/* <MemoHook /> */}
-    <MemoCounter />
+    {/* <MemoCounter /> */}
+    <InstagromApp />
   </StrictMode>,
 );
